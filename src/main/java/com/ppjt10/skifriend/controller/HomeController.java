1315de176d1fd6000cac5719b9be1ac147a0e3b1
suky0713@kyonngi.ac.kr <suky0713@kyonggi.ac.kr>
2021-12-26 22:19:54 +0900

    no message

--- src/main/java/com/ppjt10/skifriend/controller/HomeController.java
+++ src/main/java/com/ppjt10/skifriend/controller/HomeController.java
@@ -35,5 +35,4 @@ public class HomeController {
         return ResponseEntity.ok()
                 .body(boardListResponseDto);
     }
-
 }
--- src/main/java/com/ppjt10/skifriend/dto/CarpoolDto.java
+++ src/main/java/com/ppjt10/skifriend/dto/CarpoolDto.java
@@ -55,4 +55,5 @@ public class CarpoolDto {
         private String endLocation;
         private String date;
     }
+
 }
