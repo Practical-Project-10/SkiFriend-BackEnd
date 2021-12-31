aed91380e6e6d1568a772cbc1241ac66f4b4590e
SeongeunYang <yse09@naver.com>
2021-12-31 22:03:57 +0900

    /do 유저 관련 테스트 코드 작성중

--- src/test/java/com/ppjt10/skifriend/controller/UserControllerTest.java
+++ src/test/java/com/ppjt10/skifriend/controller/UserControllerTest.java
@@ -3,27 +3,40 @@ package com.ppjt10.skifriend.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ppjt10.skifriend.dto.SignupDto;
+import com.ppjt10.skifriend.dto.UserDto;
 import lombok.*;
-
 import org.junit.jupiter.api.*;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-
+@AutoConfigureMockMvc
 class UserControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
     @Autowired
     private TestRestTemplate restTemplate;
 
     private HttpHeaders headers;
+
     private static ObjectMapper objectMapper = new ObjectMapper();
 
     private String token = "";
@@ -87,11 +100,10 @@ class UserControllerTest {
 
     @Test
     @Order(3)
-    @DisplayName("회원 정보 가져오기")
+    @DisplayName("유저 정보 가져오기")
     void test3() throws JsonProcessingException {
 
         headers.set("Authorization", token);
-
         HttpEntity<Object> request = new HttpEntity<>(headers);
 
         // when
@@ -101,7 +113,30 @@ class UserControllerTest {
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
+    @Test
+    @Order(4)
+    @DisplayName("유저 프로필 작성")
+    void test4() throws Exception {
+        headers.set("Authorization", token);
+        HttpEntity<Object> request = new HttpEntity<>(headers);
 
+
+        String content = objectMapper.writeValueAsString(new UserDto.ProfileRequestDto("남","10대", "초보", "hihihi"));
+        MockMultipartFile file1 = new MockMultipartFile("profileImg", "empty.txt", "text/plain", "".getBytes());
+        MockMultipartFile file2 = new MockMultipartFile("vacImg", "empty.txt", "text/plain", "".getBytes());
+        MockMultipartFile file3 = new MockMultipartFile("requestDto", "jsondata", "application/json", content.getBytes(StandardCharsets.UTF_8));
+
+        ResultActions resultActions = mockMvc.perform(multipart("/user/profile")
+                        .file(file1)
+                        .file(file2)
+                        .file(file3).headers(headers)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8"))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        System.out.println(resultActions);
+    }
 
 
     @Getter
