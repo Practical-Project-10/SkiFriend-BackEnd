51b93888e267c8396b109d7cef5fe8fb007c120d
SeongeunYang <yse09@naver.com>
2022-01-06 13:19:37 +0900

    /fixing.. 자동 배포 s3 연결 안되는 문제 테스트 1

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,7 +9,7 @@ REPOSITORY=/home/ubuntu/app/step
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
-sudo cp $REPOSITORY/zip/*.jar $REPOSITORY/
+sudo cp $REPOSITORY/zip/skiFriend-0.0.1-SNAPSHOT.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/skiFriend-0.0.1-SNAPSHOT.jar | tail -n 1)
@@ -25,9 +25,7 @@ echo "> $JAR_NAME 실행"
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
-nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties \
-    -Dspring.profiles.active=$IDLE_PROFILE \
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties -Dspring.profiles.active=$IDLE_PROFILE \
+    $JAR_NAME &
 
 #    ,/home/ubuntu/app/application-oauth.properties,/home/ubuntu/app/application-real-db.properties
