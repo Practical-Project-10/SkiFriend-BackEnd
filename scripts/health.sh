6d6071cbac3f7950080f266db0e8cc434af775d9
SeongeunYang <yse09@naver.com>
2022-01-06 16:58:31 +0900

    /fix 인스턴스 재부팅 라우팅문제 해결, S3 실행 안되는 문제 해결중. test1

--- scripts/health.sh
+++ scripts/health.sh
@@ -8,7 +8,7 @@ source ${ABSDIR}/switch.sh
 IDLE_PORT=$(find_idle_port)
 
 echo "> Health Check Start!"
-echo "> IDLE_PORT: $IDLE_PORT test1"
+echo "> IDLE_PORT: $IDLE_PORT"
 echo "> curl -s http://localhost:$IDLE_PORT/profile "
 sleep 10
 
--- scripts/start.sh
+++ scripts/start.sh
@@ -18,13 +18,13 @@ echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-chmod +x $JAR_NAME
+sudo chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
 IDLE_PROFILE=$(find_idle_profile)
 
-echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다. test1!!!!!!!!!!!!!!!"
+echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties \
