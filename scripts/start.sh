c68d9f0c7737cf8ce935163dceaef4448d6cf3e3
SeongeunYang <yse09@naver.com>
2022-01-06 13:29:34 +0900

    /fixing.. 자동 배포 s3 연결 안되는 문제 테스트 2

--- scripts/start.sh
+++ scripts/start.sh
@@ -25,7 +25,8 @@ echo "> $JAR_NAME 실행"
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
-nohup java -jar -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties -Dspring.profiles.active=$IDLE_PROFILE \
-    $JAR_NAME &
 
-#    ,/home/ubuntu/app/application-oauth.properties,/home/ubuntu/app/application-real-db.properties
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties \
+    -Dspring.profiles.active=$IDLE_PROFILE \
+    $JAR_NAME &
