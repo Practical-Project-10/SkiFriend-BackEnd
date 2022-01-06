2b21c53d5588f4b77f600cf9f31938f0e0f6e6dc
SeongeunYang <yse09@naver.com>
2022-01-06 14:32:44 +0900

    /fixing.. 인스턴스 재부팅 시 라우팅이 엇갈리는 문제 테스트 2

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -5,9 +5,9 @@
 # 쉬고 있는 profile 찾기: real1이 사용중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
 function find_idle_profile()
 {
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
-
     SERVICE_URL=$(cat /etc/nginx/conf.d/service-url.inc | cut -c 35-38)
+
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
     if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
