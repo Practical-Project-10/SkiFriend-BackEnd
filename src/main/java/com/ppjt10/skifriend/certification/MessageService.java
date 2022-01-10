9bf9da015d9b9085afb08d4e1ab0b6d082cf46f2
SeongeunYang <yse09@naver.com>
2022-01-10 18:20:52 +0900

    /fix 메세지 기능 Off

--- src/main/java/com/ppjt10/skifriend/certification/MessageService.java
+++ src/main/java/com/ppjt10/skifriend/certification/MessageService.java
@@ -37,10 +37,10 @@ public class MessageService {
         Twilio.init(apiKey, apiSecret);
         String toPhoneNum = "+" + 82 + requestDto.getPhoneNumber();
 
-        Message message = Message.creator(
-                new PhoneNumber(toPhoneNum),
-                new PhoneNumber(fromPhoneNum),
-                SKIFRIEND + randomNum).create();
+//        Message message = Message.creator(
+//                new PhoneNumber(toPhoneNum),
+//                new PhoneNumber(fromPhoneNum),
+//                SKIFRIEND + randomNum).create();
 
         // DB에 발송한 인증번호 저장
         smsRedisRepository.createSmsCertification(requestDto.getPhoneNumber(), randomNum);
@@ -65,10 +65,10 @@ public class MessageService {
         Twilio.init(apiKey, apiSecret);
         String toPhoneNum = "+" + 82 + phoneNumber;
 
-        Message message = Message.creator(
-                new PhoneNumber(toPhoneNum),
-                new PhoneNumber(fromPhoneNum),
-                SKIFRIEND + msg).create();
+//        Message message = Message.creator(
+//                new PhoneNumber(toPhoneNum),
+//                new PhoneNumber(fromPhoneNum),
+//                SKIFRIEND + msg).create();
 
         System.out.println(phoneNumber + "에게 채팅방 생성 알림 전송 : " + msg);
     }
