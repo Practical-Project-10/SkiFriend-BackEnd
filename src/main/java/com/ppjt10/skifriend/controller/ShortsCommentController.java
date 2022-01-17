4c013d4531183971704937efd38004d8e4f78f48
mylhb9 <mylhb9@naver.com>
2022-01-17 15:50:15 +0900

    /feat shortscomment

--- src/main/java/com/ppjt10/skifriend/controller/ShortsCommentController.java
+++ src/main/java/com/ppjt10/skifriend/controller/ShortsCommentController.java
@@ -1,48 +1,60 @@
-//package com.ppjt10.skifriend.controller;
-//
-//
-//import com.ppjt10.skifriend.dto.commentdto.CommentRequestDto;
-//import com.ppjt10.skifriend.entity.User;
-//import com.ppjt10.skifriend.security.UserDetailsImpl;
-//import com.ppjt10.skifriend.service.ShortsCommentService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequiredArgsConstructor
-//public class ShortsCommentController {
-//    private final ShortsCommentService shortsCommentService;
-//
-//    // Shorts 댓글 작성
-//    @PostMapping("/board/freeBoard/{postId}/comments")
-//    public void createShortsComment(@PathVariable Long postId,
-//                              @RequestBody CommentRequestDto requestDto,
-//                              @AuthenticationPrincipal UserDetailsImpl userDetails
-//    ) {
-//        User user = userDetails.getUser();
-//        shortsCommentService.createShortsComment(postId, requestDto, user);
-//    }
-//
-//
-//    // Shorts 댓글 수정
-//    @PutMapping("/board/freeBoard/comments/{commentId}")
-//    public void updateShortsComment(@PathVariable Long commentId,
-//                              @RequestBody CommentRequestDto requestDto,
-//                              @AuthenticationPrincipal UserDetailsImpl userDetails
-//    ) {
-//        User user = userDetails.getUser();
-//        shortsCommentService.updateShortsComment(commentId, requestDto, user);
-//    }
-//
-//
-//    // Shorts 댓글 삭제
-//    @DeleteMapping("/board/freeBoard/comments/{commentId}")
-//    public void deleteShortsComment(
-//            @PathVariable Long commentId,
-//            @AuthenticationPrincipal UserDetailsImpl userDetails
-//    ) {
-//        User user = userDetails.getUser();
-//        shortsCommentService.deleteShortsComment(commentId, user);
-//    }
-//}
+package com.ppjt10.skifriend.controller;
+
+
+import com.ppjt10.skifriend.dto.commentdto.CommentRequestDto;
+import com.ppjt10.skifriend.dto.shortscommentdto.ShortsCommentRequestDto;
+import com.ppjt10.skifriend.dto.shortscommentdto.ShortsCommentResponseDto;
+import com.ppjt10.skifriend.entity.User;
+import com.ppjt10.skifriend.security.UserDetailsImpl;
+import com.ppjt10.skifriend.service.ShortsCommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ShortsCommentController {
+    private final ShortsCommentService shortsCommentService;
+
+    // Shorts 댓글 조회
+    @GetMapping("/shorts/{shortsId}")
+    public ResponseEntity<List<ShortsCommentResponseDto>> getShortsComments(@PathVariable Long shortsId) {
+        return ResponseEntity.ok().body(shortsCommentService.getShortsComments(shortsId));
+    }
+
+
+    // Shorts 댓글 작성
+    @PostMapping("/shorts/{shortsId}/comments")
+    public void createShortsComment(@PathVariable Long shortsId,
+                              @RequestBody String content,
+                              @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        User user = userDetails.getUser();
+        shortsCommentService.createShortsComment(shortsId, content, user);
+    }
+
+
+    // Shorts 댓글 수정
+    @PutMapping("/shorts/comments/{shortsCommentId}")
+    public void updateShortsComment(@PathVariable Long shortsCommentId,
+                              @RequestBody String content,
+                              @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        User user = userDetails.getUser();
+        shortsCommentService.updateShortsComment(shortsCommentId, content, user);
+    }
+
+
+    // Shorts 댓글 삭제
+    @DeleteMapping("/shorts/comments/{shortsCommentId}")
+    public void deleteShortsComment(
+            @PathVariable Long shortsCommentId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        User user = userDetails.getUser();
+        shortsCommentService.deleteShortsComment(shortsCommentId, user);
+    }
+}
--- src/main/java/com/ppjt10/skifriend/service/ShortsCommentService.java
+++ src/main/java/com/ppjt10/skifriend/service/ShortsCommentService.java
@@ -1,64 +1,105 @@
-//package com.ppjt10.skifriend.service;
-//
-//import com.ppjt10.skifriend.dto.commentdto.CommentRequestDto;
-//import com.ppjt10.skifriend.dto.shortscommentdto.ShortsCommentRequestDto;
-//import com.ppjt10.skifriend.entity.*;
-//import com.ppjt10.skifriend.repository.ShortsCommentRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//import javax.transaction.Transactional;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class ShortsCommentService {
-//    private final ShortsCommentRepository shortsCommentRepository;
-//    private final ShortsRepository shortsRepository;
-//
-//    // Shorts 댓글 작성
-//    @Transactional
-//    public void createShortsComment(Long shortsId, ShortsCommentRequestDto requestDto, User user) {
-//        Shorts shorts = shortsRepository.findById(shortsId).orElseThrow(
-//                ()-> new IllegalArgumentException("해당 shorts가 존재하지 않습니다"));
-//
-//        ShortsComment shortsComment = new ShortsComment(user.getId(), shorts, requestDto.getContent());
-//
-//        shortsRepository.save(shortsComment);
-//
-//        shorts.setShortsCommentCnt(shorts.getShortsCommentCnt() + 1);
-//    }
-//
-//    // Shorts 댓글 수정
-//    @Transactional
-//    public void updateShortsComment(Long shortsCommentId, CommentRequestDto requestDto, User user) {
-//
-//        ShortsComment comment = shortsRepository.findById(shortsCommentId).orElseThrow(
-//                () -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다")
-//        );
-//
-//        if(!user.getId().equals(comment.getUserId())) {
-//            throw new IllegalArgumentException("댓글 작성자만 댓글을 수정할 수 있습니다");
-//        }
-//
-//        comment.update(requestDto);
-//    }
-//
-//    // 자유 게시판 게시글 댓글 삭제
-//    @Transactional
-//    public void deleteComment(Long commentId, User user) {
-//
-//        Comment comment = commentRepository.findById(commentId).orElseThrow(
-//                () -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다")
-//        );
-//
-//        if(!user.getId().equals(comment.getUserId())) {
-//            throw new IllegalArgumentException("댓글 작성자만 댓글을 삭제할 수 있습니다");
-//        }
-//
-//        commentRepository.deleteById(commentId);
-//
-//        FreePost commentFreePost = comment.getFreePost();
-//
-//        commentFreePost.setCommentCnt(commentFreePost.getCommentCnt() - 1);
-//    }
-//}
+package com.ppjt10.skifriend.service;
+
+import com.ppjt10.skifriend.dto.commentdto.CommentRequestDto;
+import com.ppjt10.skifriend.dto.shortscommentdto.ShortsCommentRequestDto;
+import com.ppjt10.skifriend.dto.shortscommentdto.ShortsCommentResponseDto;
+import com.ppjt10.skifriend.entity.*;
+import com.ppjt10.skifriend.repository.ShortsCommentRepository;
+import com.ppjt10.skifriend.repository.UserRepository;
+import com.ppjt10.skifriend.time.TimeConversion;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ShortsCommentService {
+    private final ShortsCommentRepository shortsCommentRepository;
+    private final ShortsRepository shortsRepository;
+    private final UserRepository userRepository;
+
+    // Shorts 댓글 조회
+    public List<ShortsCommentResponseDto> getShortsComments(Long shortsId) {
+        Shorts shorts = shortsRepository.findById(shortsId).orElseThrow(
+                () -> new IllegalArgumentException("해당 shorts가 존재하지 않습니다")
+        );
+
+        List<ShortsCommentResponseDto> shortsCommentResponseDtos = new ArrayList<>();
+        List<ShortsComment> shortsCommentList = shortsCommentRepository.findAllByShorts(shorts);
+        for (ShortsComment shortsComment : shortsCommentList) {
+            shortsCommentResponseDtos.add(generateShortsCommentResponseDto(shortsComment));
+        }
+        return shortsCommentResponseDtos;
+    }
+        // Shorts 댓글 작성
+        @Transactional
+        public void createShortsComment (Long shortsId, String content, User user){
+            Shorts shorts = shortsRepository.findById(shortsId).orElseThrow(
+                    () -> new IllegalArgumentException("해당 shorts가 존재하지 않습니다")
+            );
+
+            ShortsComment shortsComment = new ShortsComment(user.getId(), shorts, content);
+
+            shortsRepository.save(shortsComment);
+
+            shorts.setShortsCommentCnt(shorts.getShortsCommentCnt() + 1);
+        }
+
+        // Shorts 댓글 수정
+        @Transactional
+        public void updateShortsComment (Long shortsCommentId, String content, User user){
+
+            ShortsComment shortsComment = shortsCommentRepository.findById(shortsCommentId).orElseThrow(
+                    () -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다")
+            );
+
+            if (!user.getId().equals(shortsComment.getUserId())) {
+                throw new IllegalArgumentException("댓글 작성자만 댓글을 수정할 수 있습니다");
+            }
+
+            shortsComment.update(content);
+        }
+
+        // 자유 게시판 게시글 댓글 삭제
+        @Transactional
+        public void deleteShortsComment (Long shortsCommentId, User user){
+
+            ShortsComment shortsComment = shortsCommentRepository.findById(shortsCommentId).orElseThrow(
+                    () -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다")
+            );
+
+            if (!user.getId().equals(shortsComment.getUserId())) {
+                throw new IllegalArgumentException("댓글 작성자만 댓글을 삭제할 수 있습니다");
+            }
+
+            shortsCommentRepository.deleteById(shortsCommentId);
+
+            Shorts commentShorts = shortsComment.getShorts();
+
+            commentShorts.setShortsCommentCnt(commentShorts.getShortsCommentCnt() - 1);
+        }
+
+    private ShortsCommentResponseDto generateShortsCommentResponseDto(ShortsComment shortsComment) {
+        String nickname;
+        try {
+            User user = userRepository.findById(shortsComment.getUserId()).orElseThrow(
+                    () -> new IllegalArgumentException("해당 유저가 존재하지 않습니다.")
+            );
+            nickname = user.getNickname();
+        } catch (Exception e) {
+            nickname = "알 수 없음";
+        }
+        return ShortsCommentResponseDto.builder()
+                .userId(shortsComment.getUserId())
+                .shortsCommentId(shortsComment.getId())
+                .nickname(nickname)
+                .content(shortsComment.getContent())
+                .createdAt(TimeConversion.timePostConversion(shortsComment.getCreateAt()))
+                .build();
+    }
+
+
+}
