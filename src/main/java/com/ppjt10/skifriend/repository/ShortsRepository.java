97bbd5fd0c38e08c1689428ecf8f0f007fbdde5e
mylhb9 <mylhb9@naver.com>
2022-01-17 22:34:45 +0900

    /feat 내가 쓴 shorts 목록 조회

--- src/main/java/com/ppjt10/skifriend/repository/ShortsRepository.java
+++ src/main/java/com/ppjt10/skifriend/repository/ShortsRepository.java
@@ -3,5 +3,8 @@ package com.ppjt10.skifriend.repository;
 import com.ppjt10.skifriend.entity.Shorts;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ShortsRepository extends JpaRepository<Shorts, Long> {
+    List<Shorts> findAllByUserId(Long userId);
 }
