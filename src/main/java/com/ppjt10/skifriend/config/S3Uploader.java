d9aee4726b34a995ba5a86e437a8f130e79d7763
mylhb9 <mylhb9@naver.com>
2022-01-21 00:19:02 +0900

    /fix comment api 수정

--- src/main/java/com/ppjt10/skifriend/config/S3Uploader.java
+++ src/main/java/com/ppjt10/skifriend/config/S3Uploader.java
@@ -4,8 +4,6 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.ppjt10.skifriend.entity.FreePost;
-import com.ppjt10.skifriend.repository.FreePostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
